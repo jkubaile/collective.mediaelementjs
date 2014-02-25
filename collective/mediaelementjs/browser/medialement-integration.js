cdc37f8bb548671807a0319886abb7d4c115235c
Jörg Kubaile <jk@zettwerk.com>
2014-02-25 10:05:56 +0100

    added tinymce integration with links ala collective.flowplayer and minimal translations

--- collective/mediaelementjs/browser/medialement-integration.js
+++ collective/mediaelementjs/browser/medialement-integration.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-  $('video,audio').mediaelementplayer();
+    $('span.InlineVideoOrig a, span.InlineVideoFull a').each(function() {
+        var is_full = $(this).parent().hasClass('InlineVideoFull');
+        var url = $(this).attr('href');
+
+        var video = '<video controls>';
+        video += '<source src="'+url+'" />';
+        video += '</video>';
+
+        $video = $(video);
+        $(this).replaceWith($video);
+
+        if (is_full) {
+            $wrapper = $video.wrap('<div class="videowrapper" style="width: 100%; margin 0 auto;"></div>');
+            $video.css('width', '100%');
+            $video.css('height', '100%');
+        }
+    });
+
+    $('video,audio').mediaelementplayer({
+        autoSize: true
+    });
 });
