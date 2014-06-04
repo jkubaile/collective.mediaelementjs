7a8be3c94630ae25e93df7f86c87da77027d6205
Jörg Kubaile <jk@zettwerk.com>
2014-06-04 15:26:12 +0200

    added dimension handling

--- collective/mediaelementjs/browser/medialement-integration.js
+++ collective/mediaelementjs/browser/medialement-integration.js
@@ -1,23 +1,43 @@
 $(document).ready(function () {
-    $('span.InlineVideoOrig a, span.InlineVideoFull a').each(function() {
-        var is_full = $(this).parent().hasClass('InlineVideoFull');
-        var url = $(this).attr('href');
+    var _getVideoDimension = function($element) {
+        var dimension = $element.attr('title');
+        try {
+            var width = dimension.split('x')[0];
+            var height = dimension.split('x')[1];
+        } catch (e) {
+            var width = '400';
+            var height = '300';
+        }
+        return {'width': width,
+                'height': height};
+    };
 
+    $('span.InlineVideoOrig a, span.InlineVideoFull a').each(function() {
+        var dimension = _getVideoDimension($(this));
+
+        // depends on tiny, where the class is set
+        var is_full = $(this).parent().hasClass('InlineVideoFull') || $(this).parent().hasClass('InlineVideoFull');
+
+        var url = $(this).attr('href');
         var video = '<video controls>';
         video += '<source src="'+url+'" />';
         video += '</video>';
 
         $video = $(video);
+        $video.attr('width', dimension['width']);
+        $video.attr('height', dimension['height']);
         $(this).replaceWith($video);
 
         if (is_full) {
-            $wrapper = $video.wrap('<div class="videowrapper" style="width: 100%; margin 0 auto;"></div>');
-            $video.css('width', '100%');
-            $video.css('height', '100%');
+            $video.wrap('<div class="videowrapper" style="width: 100%; margin 0 auto;"></div>');
+            var full_width = $video.parent().outerWidth();
+            var proportional_height = dimension['height'] / dimension['width'] * full_width;
+
+            $video.attr('width', full_width);
+            $video.attr('height', proportional_height);
         }
     });
 
-    $('video,audio').mediaelementplayer({
-        autoSize: true
-    });
+    $('video,audio').mediaelementplayer();
+
 });
